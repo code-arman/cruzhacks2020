1a5581f58114f94ad7cf74cef3627d82f65ce17b
Arman R <armanr123@gmail.com>
2020-01-18 23:57:27 -0800

    broken notivize but supposed to be correct

--- getrequests.js
+++ getrequests.js
@@ -1,5 +1,7 @@
 const fetch = require('node-fetch');
 const Radar = require('radar-sdk-js');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
 //import Radar from 'radar-sdk-js';
 
 function hello() {
@@ -9,15 +11,51 @@ function hello() {
       Authorization: 'prj_live_sk_29a86f86d54afb0c257cbec943597e6d395a421c'
     }
   };
-  fetch('https://api.radar.io/v1/events', obj).then(async response => {
-    let data = await response.json();
-    //if data.events[0].type changes into user.exited_geofence
-    //send sms to parent
-  });
+  let changed = false;
+  setInterval(() => {
+    fetch('https://api.radar.io/v1/events', obj).then(async response => {
+      let data = await response.json();
+      console.log(data);
+      //   console.log(data.events[0].type);
+      let info = data.events[0].type;
+
+      let randnum = Math.random() * 1;
+      console.log(randnum);
+      if (randnum > 0.5) {
+        newOne('true');
+      } else {
+        newOne('false');
+      }
+    });
+  }, 1000 * 5 * 1);
+}
+
+function newOne(userExorEnt) {
+  console.log(userExorEnt);
+  let request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (this.readyState === 4) {
+      console.log(this.status);
+      console.log(this.responseText);
+    }
+  };
+  request.open(
+    'POST',
+    'https://events-api.notivize.com/applications/33a7f3b1-8cce-473c-a7a3-ce92a3c2f0b7/event_flows/859a1363-c501-42f8-b612-2765938aca5b/events',
+    true
+  );
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(
+    JSON.stringify({
+      geofence_id: 'data',
+      type: userExorEnt
+    })
+  );
 }
 
 module.exports = {
-  hello
+  hello,
+  newOne
 };
 
 //use google to find address
