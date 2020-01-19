bbafca4c9da22ed512e9515db85c88154080b6e6
Arman R <armanr123@gmail.com>
2020-01-19 01:41:57 -0800

    just need to fix notivize

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const getrequests = require('./getrequests');
 const path = require('path');
+const Radar = require('radar-sdk-js');
+const fetch = require('node-fetch');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,8 +14,6 @@ const port = 9000;
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
-
-  // axios.get() or fetch('url', { header: 'AUTH' })
 });
 
 app.post('/', (req, res) => {
