241ae3ce719879dd0fdf8163b7b394663909cdb4
Arman R <armanr123@gmail.com>
2020-01-18 16:25:28 -0800

    ready for notivize

--- getrequests.js
+++ getrequests.js
@@ -3,17 +3,17 @@ const Radar = require('radar-sdk-js');
 //import Radar from 'radar-sdk-js';
 
 function hello() {
-  //   var obj = {
-  //     method: 'GET',
-  //     headers: {
-  //       Authorization: 'prj_live_sk_29a86f86d54afb0c257cbec943597e6d395a421c'
-  //     }
-  //   };
-  //   fetch('https://api.radar.io/v1/users ', obj).then(async response => {
-  //     let data = await response.json();
-  //     console.log(data);
-  //     //   if (response.resultCode == '200') return response.json();
-  //   });
+  var obj = {
+    method: 'GET',
+    headers: {
+      Authorization: 'prj_live_sk_29a86f86d54afb0c257cbec943597e6d395a421c'
+    }
+  };
+  fetch('https://api.radar.io/v1/events', obj).then(async response => {
+    let data = await response.json();
+    //if data.events[0].type changes into user.exited_geofence
+    //send sms to parent
+  });
 }
 
 module.exports = {
